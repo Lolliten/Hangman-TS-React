77438f640e2726f60253db9169daa1b2496c265e
Louise Blanc <142989908+Lolliten@users.noreply.github.com>
2024-10-02 08:43:19 +0200

    Working version

--- src/HangmanDrawing.tsx
+++ src/HangmanDrawing.tsx
@@ -80,16 +80,18 @@ const LEFT_LEG = (
   />
 );
 
-export function HangmanDrawing() {
+/* Array of all bodyparts in right order to be rendered */
+const BODY_PARTS = [HEAD, BODY, RIGHT_ARM, LEFT_ARM, RIGHT_LEG, LEFT_LEG];
+
+type HangmanDrawingProps = {
+  numberOfGuesses: number;
+};
+
+export function HangmanDrawing({ numberOfGuesses }: HangmanDrawingProps) {
   /* Drawing up the lines !!! */
   return (
     <div style={{ position: "relative" }}>
-      {HEAD}
-      {BODY}
-      {RIGHT_ARM}
-      {LEFT_ARM}
-      {RIGHT_LEG}
-      {LEFT_LEG}
+      {BODY_PARTS.slice(0, numberOfGuesses)}
       <div
         style={{
           height: "50px",
--- src/Keyboard.tsx
+++ src/Keyboard.tsx
@@ -1,4 +1,4 @@
-import styles from "./keyboard.module.css"
+import styles from "./Keyboard.module.css"
 
 const KEYS = [
   "a",
@@ -27,21 +27,45 @@ const KEYS = [
   "x",
   "y",
   "z",
-];
+]
 
-export function Keyboard() {
-  return;
-  <div
-    style={{
-      display: "grid",
-      gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
-      gap: ".5rem"
-    }}
-  >
-    {KEYS.map(key => {
+type KeyboardProps = {
+  disabled?: boolean
+  activeLetters: string[]
+  inactiveLetters: string[]
+  addGuessedLetter: (letter: string) => void
+}
+
+export function Keyboard({
+  activeLetters,
+  inactiveLetters,
+  addGuessedLetter,
+  disabled = false,
+}: KeyboardProps) {
+  return (
+    <div
+      style={{
+        display: "grid",
+        gridTemplateColumns: "repeat(auto-fit, minmax(75px, 1fr))",
+        gap: ".5rem",
+      }}
+    >
+      {KEYS.map(key => {
+        const isActive = activeLetters.includes(key)
+        const isInactive = inactiveLetters.includes(key)
         return (
-            <button className={styles.btn} key={key}> {key} </button>
+          <button
+            onClick={() => addGuessedLetter(key)}
+            className={`${styles.btn} ${isActive ? styles.active : ""} ${
+              isInactive ? styles.inactive : ""
+            }`}
+            disabled={isInactive || isActive || disabled}
+            key={key}
+          >
+            {key}
+          </button>
         )
-    })}
-  </div>;
+      })}
+    </div>
+  )
 }
